de174c2f0aff96664298620b6b354ee545628c9f
ThotaThanish <sriurr@gmail.com>
2017-07-30 17:46:18 +0530

    Update simpleintrest11.java

--- simpleintrest11.java
+++ simpleintrest11.java
@@ -5,9 +5,9 @@ public static void main(String[] args)
 int p=5000,r=2,t=12,omi,totalint,totalamt;
 omi=p*r/100;
 System.out.println("The One month intrest is "+omi);
-totalint=omi*tt;
+totalint=omi*t;
 System.out.println("The Total Intrest is "+totalint);
-totalamt=p+totalintt;
+totalamt=p+totalint;
 System.out.printtln("Your payable Amount is "+totalamt);
 }
 }
